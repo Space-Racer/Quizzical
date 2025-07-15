0ee03355642cdb411c214a9ab408c94a64a14bcd
coolbeanstech <coolbeanstech@outlook.com>
2025-07-15 14:11:07 -0400

    Added Multiple New Features

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_spinner_quiz_app"
+    namespace = "com.coolbeanstech.quizzical"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_spinner_quiz_app"
+        applicationId = "com.coolbeanstech.quizzical"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Amazing Quick Fire Trivia!',
+      title: 'Quizzical',
       theme: appTheme, // Apply the custom theme
       home:
       const AuthScreen(), // Directly show AuthScreen
